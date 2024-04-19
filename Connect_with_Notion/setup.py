1c32fa6ff5c79e2317e1551994900f3dd4fd0d3f
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-04-19 15:18:35 +0900

    #1 Update setup.py

--- Connect_with_Notion/setup.py
+++ Connect_with_Notion/setup.py
@@ -1,1 +1,26 @@
 # write user_data
+
+import tkinter as tk
+import os
+
+def read_file () :
+    file = "_user_info.txt"
+    
+    if not os.path.exists(file) :
+        
+        with open(file, 'w') as f :
+            f.write("token_api \ndatabase_id")
+            
+        return False
+
+    with open(file, 'r') as f :
+        data = f.read()
+
+    return True
+
+
+window = tk.Tk()
+
+window.title = ("User Info Setup")
+window.geometry("800x200")
+window.resizable(0, 0)
