a12f58405aafe6b0863ff90fcc9f42b98e45fc4b
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-04-23 21:54:00 +0900

    #2 Update main.py

--- Python_Tkinter/main.py
+++ Python_Tkinter/main.py
@@ -1,20 +1,7 @@
 import tkinter as tk
 import webbrowser 
 
-def instagram () :
-    webbrowser.open("https://www.instagram.com/")
-
-def github () :
-    webbrowser.open("https://github.com/")
-
-def baekjoon () :
-    webbrowser.open("https://www.acmicpc.net/user/")
-    
-def login () :
-    a = id_entry.get()
-    b = pw_entry.get()
-    if a == 'admin' and b == '1234' : print("Login!")
-    else : print("Error")
+import login
     
 root = tk.Tk()
 root.title("Login")
