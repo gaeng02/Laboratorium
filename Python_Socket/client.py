6a767aba57033f0436f94116d118e47fb5263d0e
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-04-28 00:20:38 +0900

    #3 Update client.py

--- Python_Socket/client.py
+++ Python_Socket/client.py
@@ -6,11 +6,13 @@ address = (host, port)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(address)
 
-while True : 
-    message = input("send: ")
-    if (message == "Q") : break
+while True :
+    try :
+        message = input("send: ")
+        if (message == "Q") : break
 
-    client_socket.send(message.encode())
+        client_socket.send(message.encode())
+    except ConnectionAbortedError : print("Socket Connection Error")
 
 
 client_socket.close()
