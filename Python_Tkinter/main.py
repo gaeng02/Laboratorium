df2f53f045e5fdc29efc5956252d331ff92e3afe
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-05-02 23:28:51 +0900

    #Proj_2 Update main.py

--- Python_Tkinter/main.py
+++ Python_Tkinter/main.py
@@ -12,17 +12,31 @@ root.title("Login")
 root.geometry("400x600")
 root.resizable(0, 0)
 
-id_label = tk.Label(root, text = "User id")
-id_label.pack() # pady=100, side = tk.TOP
-id_entry = tk.Entry(root)
-id_entry.pack()
+bg_color = "RoyalBlue1"
+fg_color = "Black"
+font = 14 
 
-pw_label = tk.Label(root, text = "Password")
+root.configure(bg = bg_color)
+
+root.grid_rowconfigure(3, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+
+id_label = tk.Label(root, text = "User id", bg = bg_color, fg = fg_color, font = font)
+id_label.grid(row = 0, column = 0, sticky="nsew")
+
+
+id_entry = tk.Entry(root)
+id_entry.pack(expand = True)
+
+pw_label = tk.Label(root, text = "Password", bg = bg_color, fg = fg_color, font = font)
 pw_label.pack()
 pw_entry = tk.Entry(root, show="*")
 pw_entry.pack()
 
 login_button = tk.Button(root, text = "Login", command = login)
 login_button.pack()
+
 
 root.mainloop()
