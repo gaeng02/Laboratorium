00805e2e2ff1f33ea2ff5734c37cacb9d1b7a68f
gaeng02 <sondin144@gmail.com>
2024-05-06 21:11:45 +0900

    #Proj_2 Update login.py

--- Python_Tkinter/login/login.py
+++ Python_Tkinter/login/login.py
@@ -4,46 +4,92 @@ from tkinter import messagebox
 def login () :
     
     def verification () : 
-        login_id = id_entry.get()
-        login_pw = pw_entry.get()
-        if (login_id == "admin") and (login_pw == "admin") : print("Login!")
-        else : login_wrong()
+        try_username = id_entry.get()
+        try_password = pw_entry.get()
+        if (try_username == "admin") and (try_password == "admin") : print("Login!")
+        else : tk.messagebox.showinfo("Login", "Check your ID and Password!")
 
-    #def join () :
 
-    def login_wrong () :
-        tk.messagebox.showinfo("Login", "Login Failed!")
-            
     root = tk.Tk()
     root.title("Login")
-    root.geometry("300x150")
+    root.geometry("300x140")
     root.resizable(0, 0)
 
-    bg_color = "RoyalBlue1"
+    bg_color = "SkyBlue1"
     fg_color = "Black"
     font = 14 
     
     root.configure(bg = bg_color)
     
-    id_label = tk.Label(root, text = "User id", bg = bg_color, fg = fg_color, font = font)
-    id_label.pack()
+    id_label = tk.Label(root, text = "Username :: ", bg = bg_color, fg = fg_color, font = font)
+    id_label.grid(row = 0, column = 0, padx = 10, pady = 10)
 
     id_entry = tk.Entry(root)
-    id_entry.pack()
+    id_entry.grid(row = 0, column = 1, padx = 10, pady = 10, columnspan = 2)
 
-    pw_label = tk.Label(root, text = "Password", bg = bg_color, fg = fg_color, font = font)
-    pw_label.pack()
+    pw_label = tk.Label(root, text = "Password :: ", bg = bg_color, fg = fg_color, font = font)
+    pw_label.grid(row = 1, column = 0, padx = 10, pady = 10)
     
     pw_entry = tk.Entry(root, show="*")
-    pw_entry.pack()
-
-    blank_label = tk.Label(root, bg = bg_color)
-    blank_label.pack()
-
+    pw_entry.grid(row = 1, column = 1, padx = 10, pady = 10, columnspan = 2)
+    
     login_button = tk.Button(root, text = "Login", command = verification)
-    login_button.pack()
+    login_button.grid(row = 2, column = 1, padx = 10, pady = 10)
 
+    join_button = tk.Button(root, text = "Create", command = create)
+    join_button.grid(row = 2, column = 2, padx = 10, pady = 10)
+    
     root.mainloop()
 
+def create () :
+
+    def create_account () :
+        try_username = id_entry.get()
+        try_password = pw_entry.get()
+        check_password = pw_again_entry.get()
+
+        if (try_password != check_password) :
+            tk.messagebox.showinfo("Create Failed", "Password and Again are not same.")
+            return ;
+        '''
+        if (!check_username(try_username)) :
+            tk.messagebox.showinfo("Create Failed", "Username is not valid.")
+        '''
+        
+        new.destroy()
+        
+    new = tk.Tk()
+    new.title("Create new Account")
+    new.geometry("300x180")
+    new.resizable(0, 0)
+
+    bg_color = "SkyBlue1"
+    fg_color = "Black"
+    font = 14 
+    
+    new.configure(bg = bg_color)
+    
+    id_label = tk.Label(new, text = "Username :: ", bg = bg_color, fg = fg_color, font = font)
+    id_label.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+    id_entry = tk.Entry(new)
+    id_entry.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+    pw_label = tk.Label(new, text = "Password :: ", bg = bg_color, fg = fg_color, font = font)
+    pw_label.grid(row = 1, column = 0, padx = 10, pady = 10)
+    
+    pw_entry = tk.Entry(new, show="*")
+    pw_entry.grid(row = 1, column = 1, padx = 10, pady = 10)
+    
+    blank_label = tk.Label(new, text = "Enter again ", bg = bg_color, fg = fg_color, font = font)
+    blank_label.grid(row = 2, column = 0, padx = 10, pady = 10)
+    
+    pw_again_entry = tk.Entry(new, show="*")
+    pw_again_entry.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+    create_button = tk.Button(new, text = "Create", command = create_account)
+    create_button.grid(row = 3, column = 1, padx = 10, pady = 10)
+    
+    new.mainloop()
 
 login()
