7ce122b1b0e0698a0a76a0cc14ef9b79e690480d
gaeng02 <sondin144@gmail.com>
2024-05-13 17:37:38 +0900

    #Proj_1 Update setup.py

--- Connect_with_Notion/setup.py
+++ Connect_with_Notion/setup.py
@@ -19,8 +19,38 @@ def read_file () :
     return True
 
 
-window = tk.Tk()
+def setup () : 
+    window = tk.Tk()
 
-window.title = ("User Info Setup")
-window.geometry("800x200")
-window.resizable(0, 0)
+    window.title = ("User Info Setup")
+    window.geometry("400x140")
+    window.resizable(0, 0)
+
+    bg_color = "SkyBlue1"
+    fg_color = "Black"
+    font = 14 
+    
+    window.configure(bg = bg_color)
+    
+    api_token_label = tk.Label(window, text = "Private API token ", bg = bg_color, fg = fg_color, font = font)
+    api_token_label.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+    api_token_entry = tk.Entry(window, width = 30)
+    api_token_entry.grid(row = 0, column = 1, padx = 10, pady = 10, columnspan = 2)
+
+    database_id_label = tk.Label(window, text = "Database ID ", bg = bg_color, fg = fg_color, font = font)
+    database_id_label.grid(row = 1, column = 0, padx = 10, pady = 10)
+    
+    database_id_entry = tk.Entry(window, width = 30)
+    database_id_entry.grid(row = 1, column = 1, padx = 10, pady = 10, columnspan = 2)
+    
+    load_button = tk.Button(window, text = "Load") # command = load
+    load_button.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+    save_button = tk.Button(window, text = "Save") # command = save
+    save_button.grid(row = 2, column = 2, padx = 10, pady = 10)
+    
+    window.mainloop()
+
+
+setup()
