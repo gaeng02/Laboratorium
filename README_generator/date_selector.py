9ffac2de546dc6987dbae562642386d81a08532d
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-05-25 23:59:14 +0900

    Proj_5 Update calender.py

--- README_generator/date_selector.py
+++ README_generator/date_selector.py
@@ -1,11 +1,22 @@
 import tkinter as tk
 
+def check (month, year) :
+    if (month < 1) :
+        month = 12
+        year -= 1
+    elif (month > 12) :
+        month %= 12
+        year += 1
+    return month, year
+
 class calender (month = 12, year = 2024) :
-    
+
     def left () :
+        month, year = check(month, year)
         calender(month - 1, year)
         
     def right () :
+        month, year = check(month, year) 
         calender(month + 1, year)
         
     m, y = int(month), int(year)
@@ -20,6 +31,12 @@ class calender (month = 12, year = 2024) :
 
     left_button.grid(row = 0, column = 0)
     right_button.grid(row = 0, column = 6)
+
+    year_label = tk.Label(root, text = year)
+    month_label = tk.Label(root, text = month)
+
+    year_label.grid(row = 0, column = 1, columnspan = 2)
+    month_label.grid(row = 0, column = 4, columnspan = 2)
     
     root.mainloop()
 
