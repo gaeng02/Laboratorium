fd9627a4da74961edc5da3d74fbc1ffcad0a4b9c
gaeng02 <sondin144@gmail.com>
2024-05-26 22:37:16 +0900

    Proj_5 Update date_selector.py

--- README_generator/date_selector.py
+++ README_generator/date_selector.py
@@ -1,28 +1,49 @@
 import tkinter as tk
 
-class calender (month = 12, year = 2024) :
-    
-    def left () :
-        calender(month - 1, year)
+class calender () :
+
+    def __init__ (self, month = 12, year = 2024) :
+        self.m = month
+        self.y = year
+        self.display()
         
-    def right () :
-        calender(month + 1, year)
+    def check (self) :
+        if (self.m < 1) :
+            self.m += 12
+            self.y -= 1
+        elif (self.m > 12) :
+            self.m -= 12
+            self.y += 1
+
+    def display(self) :
+
+        def left () :
+            self.m -= 1
+            self.check()
         
-    m, y = int(month), int(year)
-    
-    root = tk.Tk()
-    root.title("Calender")
-    root.geometry("300x140")
-    root.resizable(0, 0)
+        def right () :
+            self.m += 1
+            self.check()
+        
+        root = tk.Tk()
+        root.title("Calender")
+        root.geometry("300x140")
+        root.resizable(0, 0)
 
-    left_button = tk.Button(root, text = "◀", command = left)
-    right_button = tk.Button(root, text = "▶", command = right)
+        left_button = tk.Button(root, text = "◀", command = left)
+        right_button = tk.Button(root, text = "▶", command = right)
 
-    left_button.grid(row = 0, column = 0)
-    right_button.grid(row = 0, column = 6)
-    
-    root.mainloop()
+        left_button.grid(row = 0, column = 0)
+        right_button.grid(row = 0, column = 6)
 
-    
+        year_label = tk.Label(root, text = self.y)
+        month_label = tk.Label(root, text = self.m)
+
+        year_label.grid(row = 0, column = 1, columnspan = 2)
+        month_label.grid(row = 0, column = 4, columnspan = 2)
+
+        print(self.m, self.y)
+        
+        root.mainloop()
 
 calender()
