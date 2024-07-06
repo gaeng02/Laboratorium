34997fd57634fca5ebf442e3970dc696c82f67ef
gaeng02 <sondin144@gmail.com>
2024-07-06 15:54:52 +0900

    Elements in Prompt

--- README_generator/create_markdown.py
+++ README_generator/create_markdown.py
@@ -1,4 +1,18 @@
-def Readme (prompt : list) : 
+def Readme (prompt : list) :
+
+    '''
+    prompt
+    :: project_name : str
+    :: project_purpose : str
+    :: start_date - str (format : "yyyy - mm - dd")
+    :: last_update - str (format : "yyyy - mm - dd")
+    :: contests - bool
+    :: environment - list
+    :: member - list
+    :: library - list
+    :: instruction - list
+    '''
+        
     with open("Test.md", "w") as f :
         for item in prompt :
             f.write(str(item), end = "\n")
